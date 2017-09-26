e6aee8183a3a71fc06b9fae045dc89d5511c16e5
nox911 <nox.911@gmail.com>
2017-09-26 15:32:58 +0300

    solution with comments

--- src/index.js
+++ src/index.js
@@ -1,57 +1,67 @@
 module.exports = function zeros(expression) {
 
-var zeros=0;
-var	zeros_of_five=0;
-var	zeros_of_two=0;
-
-// Разделение на части
-var mass = expression.split('*');
-
-// Выбор по какому факториалу считать
-for (var i=0; i<mass.length;i++) {
-	var	fact=0;
-	for(var j=0;j<mass[i].length;j++) {
-			if (mass[i][j]=='!') {
-				fact++;
-			}
-	}
-	if (fact==1) {
-		rez=factorial_1(parseInt(mass[i]));
-	}
-	else {
-		rez=factorial_2(parseInt(mass[i]));
-	}	
-}
-//Подсчёт пятёрок и двоек в факториале n!
-function factorial_1 (mass) {
-	for (var i=mass;i>0;i--) {
-		if(i%5==0) {
-			zeros_of_five++;
-			if	((i/5)%5==0){
-				zeros_of_five++;
-			}
+	var param=0;
+	var zeros=0;
+	var	zeros_of_five=0;
+	var	zeros_of_two=0;
+	var mass = expression.split('*');
+	for (var i=0; i<mass.length;i++) {
+		if (mass[i][mass[i].length-1]==mass[i][mass[i].length-2]) {
+			//go to factorial(n-2)
+			factorial_2(parseInt(mass[i]));
 		}
-	if(i%2==0){
-		zeros_of_two++;
+		else {
+			//go factorial(n-1)
+			factorial_1(parseInt(mass[i]));
+		}
 	}
 
-}
-}
-//Подсчёт пятёрок и двоек в факториале n!!
-function factorial_2 (mass) {
-	for (var i=mass;i>0;i-=2) {
-		if(i%5==0) {
-			zeros_of_five++;
-			if	((i/5)%5==0){
-				zeros_of_five++;
-			}
+	//calculate whole number of five
+	function five (mass) {
+		if (mass>=5) {
+		return 	parseInt(mass/5)+five(parseInt(mass/5));
 		}
-	if(i%2==0){
-		zeros_of_two++;
+		else {
+		return 0;
+		}	
 	}
-}	
-}
-// кол-во нулей равно: минимальное количество между двойками и пятёрками
-zeros+=Math.min(zeros_of_five,zeros_of_two);
+	//calculate whole number of two
+		function two (mass) {
+		if (mass>=2) {
+		return 	parseInt(mass/2)+two(parseInt(mass/2));
+		}
+		else {
+		return 0;
+		}	
+	}
+
+	function factorial_1 (mass) {
+	zeros_of_five+=five(mass);
+	zeros_of_two+=two(mass);
+	}
+
+	function factorial_2 (mass) {
+		var p=0, // number of "except 50" in even and odd factorial
+				t=0, // number of "except 25" in even factorial
+				k=0; // number of "except between 5 and 10, 15 and 20" in even factorial
+
+		//even factorial
+		if (mass%2==0) {
+			p=parseInt(mass/50);
+			t=(parseInt(mass/25))-p;
+				if ((mass%10>=5)&&(mass<20)){
+					k=1;
+				}
+			zeros_of_five+=(Math.round(five(mass)/2))-k+p-t;
+			zeros_of_two+=two(mass);
+		}
+		else {
+		//odd factorial
+			p=parseInt(mass/50);		
+			zeros_of_five+=five(mass)-parseInt(mass/10)-p;
+		}
+	}
+	// one zero = 10 = 5*2 // find minimum between five and two
+	zeros+=Math.min(zeros_of_five,zeros_of_two);
 return zeros;
 }
